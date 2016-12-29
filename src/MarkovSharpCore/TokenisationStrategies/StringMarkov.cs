694f33c5f60e7833f1dcac6003115cdcd2f2b15e
Sindre G. Langhus <sindre_langhus@msn.com>
2016-12-30 00:54:46 +0100

    Adds the MarkovSharp project.

---
+++ src/MarkovSharpCore/TokenisationStrategies/StringMarkov.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovSharp.TokenisationStrategies
+{
+    public class StringMarkov : GenericMarkov<string, string>
+    {
+        public StringMarkov(int level = 2)
+            : base(level)
+        { }
+
+        public override IEnumerable<string> SplitTokens(string input)
+        {
+            if (input == null)
+            {
+                return new List<string>() { GetPrepadGram() };
+            }
+
+            return input?.Split(' ');
+        }
+
+        public override string RebuildPhrase(IEnumerable<string> tokens)
+        {
+            return string.Join(" ", tokens);
+        }
+
+        public override string GetTerminatorGram()
+        {
+            return null;
+        }
+
+        public override string GetPrepadGram()
+        {
+            return "";
+        }
+    }
+}
